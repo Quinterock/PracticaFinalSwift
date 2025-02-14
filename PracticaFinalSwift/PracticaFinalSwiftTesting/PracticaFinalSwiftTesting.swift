c8317b7f4ee9fe4738e28771a4c640f09ec6b870
Quinterock <luisgqr1975@gmail.com>
2025-02-13 18:29:26 -0600

    tests pt2

--- PracticaFinalSwift/PracticaFinalSwiftTesting/PracticaFinalSwiftTesting.swift
+++ PracticaFinalSwift/PracticaFinalSwiftTesting/PracticaFinalSwiftTesting.swift
@@ -9,7 +9,15 @@ import Testing
 
 struct PracticaFinalSwiftTesting {
     let main = MenuController()
+    // Verificar que hay dos usuarios por defecto
     @Test func example() async throws {
         #expect(main.users.count == 2)
+    }
+    
+    //Verificar que el primer usuario es administrador
+    @Test func verifyAdminUser() async throws {
+        let admin = MenuController()
+        let hasAdminUser = admin.users.contains {$0.role == .admin}
+        #expect(hasAdminUser)
     }
 }
